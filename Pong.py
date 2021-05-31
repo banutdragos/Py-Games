e841b41e3343d7ee679410bf3caa31abd4ce062a
banutdragos <banut_dragos@yahoo.com>
2021-05-31 15:28:05 +0300

    Implemented score tracking

--- Pong.py
+++ Pong.py
@@ -9,6 +9,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A setup
 paddle_a = turtle.Turtle()
 paddle_a.shape("square")
@@ -34,10 +38,17 @@ ball.color("white")
 ball.speed(0)
 ball.penup()
 ball.goto(0, 0)
-
 # Speed control
 ball.dx = 0.2
 ball.dy = 0.2
+
+# Pen (for score)
+pen = turtle.Turtle()
+pen.penup()
+pen.color('white')
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 | Player B: 0", align="center", font=("Courier", 20, "normal"))
 
 
 
@@ -75,13 +86,24 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+
     elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
 
-    if ball.xcor() > 390 or ball.xcor() < -390:
+    if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a +=1
+        pen.clear()
+        pen.write(f"Player A: {score_a} | Player B: {score_b}", align="center", font=("Courier", 20, "normal"))
+
+    elif ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b +=1
+        pen.clear()
+        pen.write(f"Player A: {score_a} | Player B: {score_b}", align="center", font=("Courier", 20, "normal"))
 
     # Paddle and ball collision
     if ball.xcor() > 330 and ball.xcor() < 333 and ball.ycor() < paddle_b.ycor() + 60 and ball.ycor() > paddle_b.ycor() - 60:
