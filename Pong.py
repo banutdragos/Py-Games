3c13a43825dfd346887fceeea6969e0b16fb84b8
banutdragos <banut_dragos@yahoo.com>
2021-05-31 15:56:32 +0300

    Added sound on collision

--- Pong.py
+++ Pong.py
@@ -1,6 +1,7 @@
 # Simple Pong
 
 import turtle
+import winsound
 
 # Window setup
 wn = turtle.Screen()
@@ -51,16 +52,18 @@ pen.goto(0, 260)
 pen.write("Player A: 0 | Player B: 0", align="center", font=("Courier", 20, "normal"))
 
 
-
 # Function
 def paddle_a_up():
     paddle_a.sety(paddle_a.ycor() + 20)
 
+
 def paddle_a_down():
     paddle_a.sety(paddle_a.ycor() - 20)
 
+
 def paddle_b_up():
     paddle_b.sety(paddle_b.ycor() + 20)
+
 
 def paddle_b_down():
     paddle_b.sety(paddle_b.ycor() - 20)
@@ -86,29 +89,33 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("bonk.wav", winsound.SND_ASYNC)
 
     elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("bonk.wav", winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
-        score_a +=1
+        score_a += 1
         pen.clear()
         pen.write(f"Player A: {score_a} | Player B: {score_b}", align="center", font=("Courier", 20, "normal"))
 
     elif ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
-        score_b +=1
+        score_b += 1
         pen.clear()
         pen.write(f"Player A: {score_a} | Player B: {score_b}", align="center", font=("Courier", 20, "normal"))
 
     # Paddle and ball collision
-    if ball.xcor() > 330 and ball.xcor() < 333 and ball.ycor() < paddle_b.ycor() + 60 and ball.ycor() > paddle_b.ycor() - 60:
+    if 330 < ball.xcor() < 333 and paddle_b.ycor() + 60 > ball.ycor() > paddle_b.ycor() - 60:
         ball.setx(330)
         ball.dx *= -1
-    if ball.xcor() < -330 and ball.xcor() > -333 and ball.ycor() < paddle_a.ycor() + 60 and ball.ycor() > paddle_a.ycor() - 60:
+        winsound.PlaySound("bonk.wav", winsound.SND_ASYNC)
+    if -330 > ball.xcor() > -333 and paddle_a.ycor() + 60 > ball.ycor() > paddle_a.ycor() - 60:
         ball.setx(-330)
         ball.dx *= -1
+        winsound.PlaySound("bonk.wav", winsound.SND_ASYNC)
